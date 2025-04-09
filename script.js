6752e3380ad6fa90a5cb5de2689e340a4042d334
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-09 15:34:56 +0000

    add first promise 1 of 3

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const fetch = new Promise((resolve) => {
-    setTimeout(() => resolve(console.log('hello')), 1000);
+// const fetch = new Promise((resolve) => {
+//     setTimeout(() => resolve(console.log('hello')), 1000);
+// })
+
+// fetch
+//     .then(() => console.log("fetch resolved"))
+//     .catch(() => console.error(`error: ${error}`))
+//     .finally(() => console.log('promise completed'))
+
+
+// clean the house
+const cleanHouse = new Promise((resolve, reject) => {
+    let houseCleaned = true;
+    
+    if (houseCleaned){
+        setTimeout(() => resolve(console.log("house is clean")), 1000);
+    } else {
+        reject(console.log("You have to clean the house first"));
+    }
 })
 
-fetch
-    .then(() => console.log("fetch resolved"))
-    .catch(() => console.error(`error: ${error}`))
-    .finally(() => console.log('promise completed'))
+//walk the dog
+
+// take out trash
+
+//go play with friend 
