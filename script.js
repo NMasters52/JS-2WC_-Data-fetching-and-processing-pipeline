272545744aa24bc3e7cfe7709d14c176f2c30a44
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-09 16:10:18 +0000

    will have to refractor

--- script.js
+++ script.js
@@ -13,14 +13,54 @@ const cleanHouse = new Promise((resolve, reject) => {
     let houseCleaned = true;
     
     if (houseCleaned){
-        setTimeout(() => resolve(console.log("house is clean")), 1000);
-    } else {
-        reject(console.log("You have to clean the house first"));
-    }
+        setTimeout(() => {
+            console.log("house is clean")
+            resolve()
+        }
+      , 1000);
+     } else {
+        reject("You have to clean the house first")
+ }
 })
 
 //walk the dog
+const walkDog = new Promise((resolve, reject) => {
+    let dogWalked = true;
+
+    if (dogWalked) {
+        setTimeout(() => {
+            console.log('dog is walked')
+            resolve();
+        }
+        , 6000)
+    } else {
+        reject('dog has to be walked')
+    }
+})
 
 // take out trash
+const takeOutTrash = new Promise ((resolve, reject) => {
+ let trashTakenOut = true;
 
+    if(trashTakenOut) {
+        setTimeout(() => {
+            console.log('trash is taken out')
+            resolve();
+        }, 1000)
+        
+    } else {
+        reject('the trash has not been taken out')
+    }
+})
+
+cleanHouse
+    .then(() => {
+        return walkDog
+    })
+    .then(() => {
+        return takeOutTrash
+    })
+    .then(() => console.log('chores are complete'))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('you can go play with your friends now'))
 //go play with friend 
