d88e50cf05f42d25db730c029ef46d3304af4ddc
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-15 15:37:21 +0000

    add error handling to async func

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 async function fetchPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/clefairy/');
-    const data = await response.json()
-    console.log(data.weight)
+
+    try{
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/clefairy/');
+        
+        if (!response.ok){
+            console.log('error fetching data');
+        }
+
+        const data = await response.json()
+        console.log(data.weight)
+
+    } catch(error) {
+        console.error(error);
+    }
+    
 }
+
+fetchPokemon()
