ea04202182fb0b9a550478f75111883a61ba2055
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-15 15:55:27 +0000

    add dynamically searching pokemon by user input

--- script.js
+++ script.js
@@ -4,23 +4,46 @@ const elements = {
     searchDisplay: document.getElementById('searchDisplay')
 }
 
-
-async function fetchPokemon() {
+ async function search() {
+    const pokemonName = elements.searchBar.value;
 
     try{
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/clefairy/');
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
         
         if (!response.ok){
             console.log('error fetching data');
         }
 
         const data = await response.json()
-        console.log(data.weight)
+        elements.searchDisplay.innerHTML = `
+        <p>${data.weight}</p>`
 
     } catch(error) {
         console.error(error);
     }
-    
 }
 
-fetchPokemon()
+
+// async function fetchPokemon() {
+
+//     try{
+//         const response = await fetch('https://pokeapi.co/api/v2/pokemon/clefairy/');
+        
+//         if (!response.ok){
+//             console.log('error fetching data');
+//         }
+
+//         const data = await response.json()
+//         console.log(data.weight)
+
+//     } catch(error) {
+//         console.error(error);
+//     }
+    
+// }
+
+function initializeApp() {
+    elements.searchBtn.addEventListener('click', search)
+}
+
+initializeApp();
