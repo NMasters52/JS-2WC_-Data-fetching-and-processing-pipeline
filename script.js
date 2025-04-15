1ffafe6cad166fd9026c8768485a05e3ed4ec62a
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-15 15:58:11 +0000

    refractor how fetched data was displayed on page

--- script.js
+++ script.js
@@ -15,32 +15,18 @@ const elements = {
         }
 
         const data = await response.json()
-        elements.searchDisplay.innerHTML = `
-        <p>${data.weight}</p>`
+        displaySearch(data)
 
     } catch(error) {
         console.error(error);
     }
 }
 
+function displaySearch(fetchResponseData) {
+    elements.searchDisplay.innerHTML = `
+        <p>${fetchResponseData.weight}</p>`
+}
 
-// async function fetchPokemon() {
-
-//     try{
-//         const response = await fetch('https://pokeapi.co/api/v2/pokemon/clefairy/');
-        
-//         if (!response.ok){
-//             console.log('error fetching data');
-//         }
-
-//         const data = await response.json()
-//         console.log(data.weight)
-
-//     } catch(error) {
-//         console.error(error);
-//     }
-    
-// }
 
 function initializeApp() {
     elements.searchBtn.addEventListener('click', search)
